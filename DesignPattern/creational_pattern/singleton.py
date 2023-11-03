def7fa9e9d79b283550db665c66ce5c1be4339cd
salahuddin975 <salahuddin975@gmail.com>
2023-11-03 08:04:19 -0500

    Add python implementation for the Singleton design pattern

---
+++ DesignPattern/creational_pattern/singleton.py
@@ -0,0 +1,26 @@
+class Singleton:
+    _instance = None      # Private variable to hold the single instance
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Singleton, cls).__new__(cls)    # creates a new instance
+            cls._instance.init_singleton()    # perform any initialization here
+        return cls._instance 
+
+    def init_singleton(self):
+        self.value = 0    # example attribute for the singleton
+
+s1 = Singleton()
+s2 = Singleton()
+s1.value = 10
+print(s1.value)
+print(s2.value)
+print(s1 is s2)
+
+
+'''
+- The Singleton class defines a private class variable _instance to hold the single instance of the class.
+- The __new__ method is overridden to control the creation of instances. It checks whether _instance is already set. If not, it creates a new instance and assigns it to _instance. If _instance already exists, it returns the existing instance.
+- The init_singleton method can be used for any initialization logic for the singleton.
+- In the usage section, you create two instances s1 and s2, but they both refer to the same underlying object. Modifying the value attribute of s1 also changes the value attribute of s2 because they share the same instance.
+'''
